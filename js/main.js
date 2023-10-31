36ba031f4634b50a2226157ae07056bdb23dc507
Tommy Melançon-Roy <tmelanconroy@gmail.com>
2023-10-31 17:13:53 -0400

    improve pictures section with modal

--- js/main.js
+++ js/main.js
@@ -65,3 +65,22 @@ navLinks.forEach((l) => {
     l.addEventListener('click', () => { bsCollapse.toggle() })
 });
 
+
+const exampleModal = document.getElementById('picture-modal')
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalBodyInput = exampleModal.querySelector('.modal-body img')
+
+    modalBodyInput.src = recipient
+  })
+}
+
+
